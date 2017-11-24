8531c36f422652837c5cd5f31cc04ffbca03d44a
f14shm4n <uraborg@gmail.com>
2017-11-24 19:18:18 +0300

    up

---
+++ f14-android-common/src/main/java/f14/android/ErrorReporter.java
@@ -0,0 +1,41 @@
+package f14.android;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import f14.common.ExHelper;
+
+/**
+ * Created by f14shm4n on 22.11.2017.
+ */
+
+public final class ErrorReporter {
+    public static void sendErrorReport(Context context, Class<?> intentClass, Exception e) {
+        Intent resultIntent = new Intent(context, intentClass);
+        resultIntent.putExtra("EX_DATA", e.toString());
+        resultIntent.putExtra("EX_STACK", ExHelper.stackTraceToString(e));
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(resultIntent);
+
+//                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(BTBackgroundService.this)
+//                        .setSmallIcon(R.drawable.appicon)
+//                        .setContentTitle("Error")
+//                        .setContentText(e.getMessage());
+//
+//                Intent resultIntent = new Intent(BTBackgroundService.this, ExActivity.class);
+//                resultIntent.putExtra("EX_DATA", e.toString());
+//                resultIntent.putExtra("EX_STACK", ExHelper.stackTraceToString(e));
+//
+//                TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//                stackBuilder.addParentStack(ExActivity.class);
+//                stackBuilder.addNextIntent(resultIntent);
+//
+//                PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//                mBuilder.setContentIntent(resultPendingIntent);
+//
+//                ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(1, mBuilder.build());
+
+    }
+}
